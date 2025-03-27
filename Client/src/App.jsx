587c69f0fd3fc01728923d1b8afdd9e7929c0794
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-03-27 12:22:13 +0530

    changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 function App() {
   return (
 
-    <div>
+    <div className="App">
 
       <Header />
+
+
+      <Footer/>
 
     </div>
   )
--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Header() {
-    return (
-        <div>Header</div>
-    )
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default Header
