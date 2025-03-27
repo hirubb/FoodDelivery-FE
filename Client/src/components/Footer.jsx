297986fb10b2a39bc2583434ecce1e16d4c1396f
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-03-27 12:22:13 +0530

    changes

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Footer() {
+export default function Footer() {
     return (
-        <div>Footer</div>
+        <div>
+            <h1>
+            Footer
+                
+            </h1></div>
     )
 }
 
-export default Footer
