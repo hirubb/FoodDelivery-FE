03f75e333597d8cdd8d925ea96d526b38f6beb71
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-03-27 13:02:25 +0530

    final folder structure

---
+++ Client/src/Pages/Home/DeliveryStats.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DeliveryStats() {
+  return (
+    <div>
+       <h1>DeliveryStats</h1>
+      
+    </div>
+  )
+}
+
+export default DeliveryStats
---
+++ Client/src/Pages/Home/StartOrdering.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StartOrdering() {
+  return (
+    <div>
+      <h1>StartOrdering</h1>
+    </div>
+  )
+}
+
+export default StartOrdering
