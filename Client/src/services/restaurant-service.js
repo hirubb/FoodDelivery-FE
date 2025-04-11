18e0100f4abfa57365fa8832750ac5664dd5c672
hirun athukorala <hirun.athukorala@arctiq.com.au>
2025-04-11 18:53:57 +0530

    menu creation

--- Client/src/services/restaurant-service.js
+++ Client/src/services/restaurant-service.js
@@ -13,6 +13,25 @@ class RestaurantService {
   getRestaurantOwner(){
     return HTTP.get("/restaurant-owners/my-details");
   }
+  getMyRestaurants(){
+    return HTTP.get("/restaurant/my-restaurants");
+  }
+  login(formData){
+    return HTTP.post("/restaurant-owners/login",formData)
+  }
+  getMenus(restaurant_id){
+    console.log("service calleddd")
+    return HTTP.get(`menu/${restaurant_id}`);
+  }
+
+  AddMenu(formData){
+    
+    return HTTP.post("menu/create",formData)
+  }
+  AddMenuItems(formData){
+    console.log("create called");
+    return HTTP.post("menu-item/create",formData)
+  }
 }
 
 export default new RestaurantService();
