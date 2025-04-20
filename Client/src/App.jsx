520512c6d408aaa32a2dfe984ec83bdf2bba74e6
Dewmini Nawodya <dewmininawodya26@gmail.com>
2025-04-20 13:06:00 +0530

    delete payment

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,15 +12,7 @@ import RestaurantDetails from './Pages/Order/RestaurantDetails';
 import Cart from './Pages/Order/Cart';
 import { AuthProvider } from './components/Auth/AuthContext';
 import Profile from './Pages/Restaurant/profile/Profile';
-import PaymentMethodSelectionPage from './Pages/Payment/PaymentMethodSelectionPage'
-import PaymentMethodSelector from './components/Payment/PaymentMethodSelector'
-import OrderSummary from './components/Payment/OrderSummary'
-import CheckoutCardPage from './Pages/Payment/CheckoutCardPage'
-import PaypalPayment from './Pages/Payment/PaypalPayment'
-import PaymentSuccess from './Pages/Payment/PaymentSuccess'
-import PaymentFailed from './Pages/Payment/PaymentFailed'
-import CardPaymentForm from './components/Payment/CardPaymentForm'
-import PaymentRefunded from './Pages/Payment/PaymentRefunded'
+
 
 // AppContent should be inside Router to use useLocation()
 const AppContent = () => {
@@ -41,15 +33,7 @@ const AppContent = () => {
           <Route path="/restaurant/:id" element={<RestaurantDetails />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/owner/profile" element={<Profile />} />
-          <Route path="/payment/selectpaymentmethod" element={<PaymentMethodSelectionPage />} />
-          <Route path="/payment/paymentmethodselector" element={<PaymentMethodSelector />} />
-          <Route path="/payment/ordersummary" element={<OrderSummary />} />
-          <Route path="/payment/checkoutcard" element={<CheckoutCardPage />} />
-          <Route path="/payment/paypal-payment" element={<PaypalPayment />} />
-          <Route path="/payment/payment-success" element={<PaymentSuccess />} />
-          <Route path="/payment/payment-failed" element={<PaymentFailed />} />
-          <Route path="/payment/cardpayment-form" element={<CardPaymentForm />} />
-          <Route path="/payment/payment-refunded" element={<PaymentRefunded />} />
+          
         </Routes>
       </main>
       {!isAuthPage && <Footer />}
