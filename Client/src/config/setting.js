9ac26c2b9dabf95e73440424ac2af217110227e0
Dewmini Nawodya <dewmininawodya26@gmail.com>
2025-04-24 00:06:24 +0530

    payments

--- Client/src/config/setting.js
+++ Client/src/config/setting.js
@@ -3,3 +3,4 @@ export const adminURL = import.meta.env.VITE_ADMIN_URL;
 export const authURL = import.meta.env.VITE_AUTH_URL;
 export const customerURL = import.meta.env.VITE_CUSTOMER_URL;
 export const DeliveryRider_BaseURL = import.meta.env.VITE_Delivery_URL;
+export const paymentURL = import.meta.env.VITE_PAYMENT_URL;
