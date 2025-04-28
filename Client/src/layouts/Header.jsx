45bd072ce65ac87749f7d1b5bc4c25abf3b03134
DulmiKaushalya <dulmikaushalya02@gmail.com>
2025-04-28 17:49:35 +0530

    add css order service

--- Client/src/layouts/Header.jsx
+++ Client/src/layouts/Header.jsx
@@ -5,7 +5,7 @@ import logo from "/src/assets/logo-color.png";
 import Sidebar from "./Sidebar";
 import { UserContext } from "../context/UserContext";
 import { FaUserCircle } from "react-icons/fa";
-import { CartContext } from "../context/CartContext";
+
 
 function Header() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -35,8 +35,6 @@ function Header() {
     setSidebarOpen(!sidebarOpen);
   };
 
-const { getCartCount, setIsCartOpen } = useContext(CartContext);
-const cartItemCount = getCartCount();
 
 
   return (
@@ -103,14 +101,14 @@ const cartItemCount = getCartCount();
             )}
 
             {/* Cart Icon */}
-            <button onClick={() => setIsCartOpen(true)} className="relative">
-                <FaShoppingCart size={24} className="text-white" />
-                {cartItemCount > 0 && (
-    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-      {cartItemCount}
-    </span>
-  )}
-</button>
+            <Link to="/cart" className="relative">
+              <FaShoppingCart size={24} className="text-white" />
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
+                3
+              </span>
+            </Link>
+
+              
 
             {/* Menu Icon */}
             <button
